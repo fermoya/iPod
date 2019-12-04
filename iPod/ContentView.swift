c371e8de613cb02a7e5123ec489447f8be05bff0
Fernando Moya de Rivas <fernando.moyaderivas@hostelworld.com>
2019-12-04 10:54:31 +0000

    Initial Commit

---
+++ iPod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPod
+//
+//  Created by Fernando Moya de Rivas on 04/12/2019.
+//  Copyright © 2019 Fernando Moya de Rivas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
